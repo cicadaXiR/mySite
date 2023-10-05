75c27ac0216b3a8a647e82a1096700d34d764308
cicadaXiR <89849656+cicadaXiR@users.noreply.github.com>
2023-10-05 11:16:37 +0530

    New pages are added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home";
+import DSA from "./pages/DSA";
+import Java from "./pages/Java";
+import Reactjs from "./pages/Reactjs";
+import Spring from "./pages/Spring";
+import Algorithms from "./pages/Algorithms";
 
 function App() {
   return (
     <div>
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dsa" element={<DSA />} />
+          <Route path="/java" element={<Java />} />
+          <Route path="/react" element={<Reactjs />} />
+          <Route path="/spring" element={<Spring />} />
+          <Route path="/algorithms" element={<Algorithms />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import '../css/home.css';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div>
-      
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/dsa">DSA</Link></li>
+        <li><Link to="/java">Java</Link></li>
+        <li><Link to="/react">ReactJs</Link></li>
+        <li><Link to="/spring">Spring</Link></li>
+        <li><Link to="/algorithms">Algorithm</Link></li>
+        <li style={{float:"right"}}><a class="active" href="#about">About</a></li>
+      </ul>
     </div>
   )
 }
