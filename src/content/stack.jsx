25f46a4a55d98ba56193d7ab0a41870f99e0a078
cicadaXiR <89849656+cicadaXiR@users.noreply.github.com>
2023-10-09 17:16:46 +0530

    added hree more pages

---
+++ src/content/stack.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function stack() {
+  return (
+    <div>
+      <h2>DSA - Stack</h2>
+    </div>
+  )
+}
+
+export default stack
--- src/pages/DSA.jsx
+++ src/pages/DSA.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
 import '../css/home.css';
+import { Link, Routes, Route } from "react-router-dom"
+import Stack from './../content/stack';
+import Queue from '../content/Queue';
 
 function DSA() {
     return (
         <>
             <div class='dsa'>
-                <a href="#about">DSA - Stack</a>
-                <a href="#services">DSA - Queue</a>
-                <a href="#clients">DSA - Tree</a>
-                <a href="#contact">DSA - Graph</a>
+                <Link to="/stack">DSA - Stack</Link>
+                <Link to="/queue">DSA - Queue</Link>
+                <a href="#">DSA - Tree</a>
+                <a href="#">DSA - Graph</a>
             </div>
             <div class="main">
-                <h2>Auto Sidebar</h2>
-                <p>This sidebar is as tall as its content (the links), and is always shown.</p>
-                <p>Scroll down the page to see the result.</p>
-                <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-                <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-                <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-                <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-            </div>
+            <Routes>
+                <Route path="/stack" remder={()=><Stack/>} />
+                <Route path="/queue" Component={Queue} />
+            </Routes>
+        </div >
         </>
     )
 }
