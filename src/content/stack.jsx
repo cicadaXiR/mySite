f67afb57ec26ce27e147fba046d758c9930bb6be
cicadaXiR <89849656+cicadaXiR@users.noreply.github.com>
2023-10-10 14:59:09 +0530

    added a new image

--- src/content/stack.jsx
+++ src/content/stack.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import image from "../images/stack.jpg"
 function stack() {
   return (
-    <div>
-      <h2>DSA - Stack</h2>
+    <div className='container'>
+      <h1>DSA - Stack</h1>
+      <br />
+      <br />
+      <p className='text-justify'>
+        <h2>Overview</h2>
+        Stack is kind of data structure which allows operations on data only at one end. It allows access to the last inserted data only. 
+        Stack is also called LIFO (Last In First Out) data structure and Push and Pop operations are related in such a way that only last item pushed (added to stack) can be popped (removed from the stack)
+      </p>
+      <br/>
+      <h2>Stack Representation</h2>
+      <br/>
+      <div align="center">
+        <img src={image} alt='stack'/>
+      </div>
+      
     </div>
   )
 }
