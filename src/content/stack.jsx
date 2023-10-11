041af239d0c04d50e631e4cf6db42d062e78e98e
cicadaXiR <89849656+cicadaXiR@users.noreply.github.com>
2023-10-11 12:55:38 +0530

    Modifications in stack.jsx file

--- src/content/stack.jsx
+++ src/content/stack.jsx
@@ -1,39 +1,26 @@
 import React, { useState } from 'react'
 import image from "../images/stack.jpg"
 import '../css/demo.css';
-import axios from 'axios';
 
-function stack() {
-  const [stack,setStack] = useState([]);
-  const [itemToAdd, setItemToAdd] = useState("");
-  const [apiData, setApiData] = useState([]);
+function Stack() {
+  const [stack, setStack] = useState([]);
+  const [input, setInput] = useState('');
 
-  useEffect(()=>{
-    axios.post("")
-      .then((response)=>{
-        setApiData(response.data);
-      })
-      .catch((error)=>{
-        console.log("Error with Api",error);
-      });
-  },[]);
-
-  const pushItem = () =>{
-    if(itemToAdd.trim() === ""){
-        return;
+  const pushItem = () => {
+    if (input.trim() !== '') {
+      setStack([stack, input]);
+      setInput('');
     }
-    setStack[stack,itemToAdd];
-    setItemToAdd("");
-  }
+  };
 
-  const popItem = () =>{
-    if(stack.length == 0){
+  const popItem = () => {
+    if (stack.length === 0) {
       return;
     }
     const newStack = [stack];
     newStack.pop();
     setStack(newStack);
-  }
+  };
   return (
     <div className='container'>
       <h1>DSA - Stack</h1>
@@ -41,28 +28,36 @@ function stack() {
       <br />
       <p className='text-justify'>
         <h2>Overview</h2>
-        Stack is kind of data structure which allows operations on data only at one end. It allows access to the last inserted data only. 
+        Stack is kind of data structure which allows operations on data only at one end. It allows access to the last inserted data only.
         Stack is also called LIFO (Last In First Out) data structure and Push and Pop operations are related in such a way that only last item pushed (added to stack) can be popped (removed from the stack)
       </p>
-      <br/>
+      <br />
       <h2>Stack Representation</h2>
-      <br/>
+      <br />
       <div align="center">
-        <img src={image} alt='stack'/>
+        <img src={image} alt='stack' />
       </div>
-      <br/>
+      <br />
       <h2>Stack Demo</h2>
-      <br/>
+      <br />
       <div class="stackDemo">
-        <input type="text" placeholder='Enter the item' value={itemToAdd}
-        onChange={(e)=>{e.target.value}}/>
-        <br/>
-        <button onClick={pushItem}>Push</button>
-        <br/>
-        <button onClick={popItem}>Push</button>
+        <input type="text" placeholder='Enter the item' value={input}
+          onChange={(e) => setInput(e.target.value)} />
+        &nbsp; &nbsp; &nbsp;
+        <button className='btn btn-success' onClick={() => pushItem(Math.random())}>Push</button>
+        &nbsp; &nbsp;
+        <button className='btn btn-danger' onClick={popItem}>Pop</button>
+        <div>
+          <strong>Stack:</strong>
+          <ul>
+            {stack.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   )
 }
 
-export default stack
+export default Stack
