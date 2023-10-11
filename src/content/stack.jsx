ec2cc2ea00edefaec01ec4c50dd5a958088b513f
cicadaXiR <89849656+cicadaXiR@users.noreply.github.com>
2023-10-11 10:54:56 +0530

    modifications in mysite project

--- src/content/stack.jsx
+++ src/content/stack.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import image from "../images/stack.jpg"
 import '../css/demo.css';
+import axios from 'axios';
 
 function stack() {
+  const [stack,setStack] = useState([]);
+  const [itemToAdd, setItemToAdd] = useState("");
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(()=>{
+    axios.post("")
+      .then((response)=>{
+        setApiData(response.data);
+      })
+      .catch((error)=>{
+        console.log("Error with Api",error);
+      });
+  },[]);
+
+  const pushItem = () =>{
+    if(itemToAdd.trim() === ""){
+        return;
+    }
+    setStack[stack,itemToAdd];
+    setItemToAdd("");
+  }
+
+  const popItem = () =>{
+    if(stack.length == 0){
+      return;
+    }
+    const newStack = [stack];
+    newStack.pop();
+    setStack(newStack);
+  }
   return (
     <div className='container'>
       <h1>DSA - Stack</h1>
@@ -23,7 +54,12 @@ function stack() {
       <h2>Stack Demo</h2>
       <br/>
       <div class="stackDemo">
-        <input type="text" placeholder='Enter the item'/>
+        <input type="text" placeholder='Enter the item' value={itemToAdd}
+        onChange={(e)=>{e.target.value}}/>
+        <br/>
+        <button onClick={pushItem}>Push</button>
+        <br/>
+        <button onClick={popItem}>Push</button>
       </div>
     </div>
   )
